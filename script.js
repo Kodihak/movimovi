6ec5b7db8eff17a1a22bdf3e7c279acbddd682c7
Matt Bennett <mattdavidbennett@gmail.com>
2015-11-24 22:01:28 -0500

    Initial

---
+++ script.js
@@ -0,0 +1,74 @@
+var url, data;
+var id = 1;
+var MAX_COUNT = 1;
+var MAX_COUNT_PERCENT = MAX_COUNT / 100;
+var count = 0;
+var movies = new Array();
+var startTime, endTime;
+var cumulativeTime = 0;
+var TOTAL_MOVIES = 3500000;
+
+function pad(n, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= 7 ? n : new Array(7 - n.length + 1).join(z) + n;
+}
+
+function sendRequests() {
+    for (var i = 0; i < MAX_COUNT_PERCENT; i++) {
+        url = 'http://www.omdbapi.com/?i=tt' + pad(id) + '&r=json';
+
+        $.ajax(url, {
+            complete: function (xhr, status) {
+                if (status === "success") {
+                    data = $.parseJSON(xhr.responseText);
+
+                    var titleIsValidMovie = (typeof (data.Title) != "undefined");
+                    titleIsValidMovie &= data.Type === "movie";
+                    titleIsValidMovie &= (data.Rated === "R" || data.Rated === "PG-13" || data.Rated === "PG" || data.Rated === "G");
+
+                    if (typeof (data.imdbVotes) != "undefined") {
+                        titleIsValidMovie &= (parseFloat(data.imdbVotes.replace(/,/g, '')) >= 2000);
+                    } else {
+                        titleIsValidMovie = false;
+                    }
+
+                    if (titleIsValidMovie) {
+                        movies.push(data);
+                    }
+                }
+            }
+        });
+
+        id++;
+    }
+}
+
+$(document).ajaxComplete(function (event, xhr, settings) {
+    count++;
+
+    if (count == 1) {
+        console.log("Beginning search...");
+        startTime = Date.now();
+    } else if (count % MAX_COUNT_PERCENT == 0 && count != MAX_COUNT) {
+        var percent = count / MAX_COUNT_PERCENT;
+        var time = Date.now() - startTime;
+        cumulativeTime += time;
+        startTime = Date.now();
+        var seconds = (time / 1000).toFixed(3);
+        var x = 'X'.repeat(percent);
+        var dot = '.'.repeat(100 - percent);
+        console.log(percent + '% [' + x + dot + ']   Seconds passed: ' + seconds + ' Cumulative time: ' + (cumulativeTime / 1000).toFixed(3) + ' Average seconds: ' + (cumulativeTime / percent / 1000).toFixed(3));
+        sendRequests();
+    } else if (count == MAX_COUNT) {
+        endTime = Date.now();
+        var time = endTime - startTime;
+        console.log("Processed " + MAX_COUNT + " movies in " + (time / 1000).toFixed(2) + " seconds.");
+        console.log("Found " + movies.length + " viable movies.");
+        $('#response').html(JSON.stringify(movies));
+        console.log(sizeof(JSON.stringify(movies)));
+    }
+});
+
+// BEGIN
+sendRequests();
